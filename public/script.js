4b7a46e892a84f809d7157beec1fc5af822f8514
ninjap4nd40 <huuskoneneino@gmail.com>
2025-02-05 18:13:39 +0200

    5.2

--- public/script.js
+++ public/script.js
@@ -1,6 +1,39 @@
+let BOARD_SIZE = 15;
+let board; //kenttä tallennetaan tähän
+
+
+
+
 document.getElementById("new-game-btn").addEventListener('click', startGame)
 
 function startGame(){
     document.getElementById('intro-screen').style.display = 'none';
     document.getElementById('game-screen').style.display = 'block';
+
+    board = generateRandomBoard();
+
+}
+
+function getCell(board, x, y){
+    return board [y][x];
+}
+
+function generateRandomBoard(){
+
+    const newBoard = Array.from({ length: BOARD_SIZE}, () =>
+        Array(BOARD_SIZE).fill(' '));
+
+    console.log(newBoard);
+
+    for(let y=0; y < BOARD_SIZE; y++){
+        for(let x=0; x < BOARD_SIZE; x++){
+            if (y === 0 || y === BOARD_SIZE-1 || x === 0 || x === BOARD_SIZE-1)
+            {
+                newBoard[y][x] = 'W' //W on wall
+            }
+        }
+    }
+
+    return newBoard;
+
 }
